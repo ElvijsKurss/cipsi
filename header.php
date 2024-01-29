f0ec32378733eb1885ab7d9b7fb4fb01c6e4e8f6
ElvijsKurss <elvijs.kurss@gmail.com>
2024-01-29 16:06:58 +0200

    123

--- header.php
+++ header.php
@@ -7,9 +7,20 @@
 </head>
 
 <body>
-    <header>
+    <header class="header">
         <nav id="navigation-bar">
-
+            <div id="header-container">
+                <div id="logo">Garšīga kompānija</div>
+                <div id="list-container">
+                    <ul id="list">
+                        <li class="list-element">Jaunumi</li>
+                        <li class="list-element">Produkti</li>
+                        <li class="list-element">Ilgtspēja</li>
+                        <li class="list-element">Uzturklase</li>
+                        <li class="list-element">Par mums</li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     </header>
     <div id="main-content">
